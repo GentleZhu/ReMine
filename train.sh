7f3c38de38b84a7699b1c92e3ae5243955405f42
GentleZhu <544524199@qq.com>
2018-02-17 01:24:25 -0600

    test

--- train.sh
+++ train.sh
@@ -5,11 +5,8 @@ python3 src_py/distantSupervision.py --op=relationLinker --in1=data/nyt/train_ny
 
 mkdir -p tmp_remine/
 echo ${green}===Tokenizaztion===${reset}
-#python3 src_py/preprocessing.py --op=train --in1=$TWT_DIR/lemmasTotal.txt --in2=$TWT_DIR/posTotal.txt --in3=$TWT_DIR/depTotal.txt
 python3 src_py/preprocessing.py --op=train --in1=data/nyt/total.lemmas.txt --in2=data/nyt/total.pos.txt --in3=data/nyt/total.dep.txt
-#python3 src_py/preprocessing.py --op=test --in1=$KBP_DIR/test.lemmas.txt --in2=$KBP_DIR/test.pos.txt --in3=$KBP_DIR/test.dep.txt
 python3 src_py/preprocessing.py --op=test --in1=data/nyt/test.lemmas.txt --in2=data/nyt/test.pos.txt --in3=data/nyt/test.dep.txt
-#python3 src_py/preprocessing.py --op=chunk --in1=$TWT_DIR/lemmasTotal.txt --in2=$TWT_DIR/posTotal.txt
 python3 src_py/preprocessing.py --op=chunk --in1=data/nyt/total.lemmas.txt --in2=data/nyt/total.pos.txt
 python3 src_py/preprocessing.py --op=translate --in1=data/stopwords.txt --out=tmp_remine/tokenized_stopwords.txt
 
