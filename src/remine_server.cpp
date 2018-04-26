336e9ecd648cfb2c663dc460eb0ca930a8004934
YicongY <able1994@hotmail.com>
2018-04-25 21:17:28 -0500

    add

--- src/remine_server.cpp
+++ src/remine_server.cpp
@@ -129,8 +129,8 @@ int main(int argc, char *argv[])
         std::cout<<"BEGIN\n";
         vector<string> files;
         vector<string> tokens;
-        files = split(TEXT_TO_SEG_REMINE,'\t');
-        tokens = split(TEXT_TO_SEG_REMINE,'\n');
+        files = split(TEXT_TO_SEG_REMINE,'\t\t');
+        tokens = split(TEXT_TO_SEG_REMINE,'\t');
         for(string vertex: files){
             std::cout << vertex << " ";
         }
