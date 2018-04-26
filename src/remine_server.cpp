389f4d887fe5866c13d50dc0bfb26d408838d654
YicongY <able1994@hotmail.com>
2018-04-25 22:53:12 -0500

    add

--- src/remine_server.cpp
+++ src/remine_server.cpp
@@ -119,7 +119,7 @@ int main(int argc, char *argv[])
         //std::cout<<TEXT_TO_SEG_DEPS_REMINE;
         //std::cin>>TEST_EMS_REMINE;
         //std::cout<<TEST_EMS_REMINE;
-        //
+        int num_line;
         std::cin>>num_line;
         std::getline (std::cin,TEXT_TO_SEG_REMINE);
         std::cout<< TEXT_TO_SEG_REMINE;
