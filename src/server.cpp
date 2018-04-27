1d4af725e29f751f17d829335616ae3287a9277c
YicongY <able1994@hotmail.com>
2018-04-27 13:53:47 -0500

    add

---
+++ src/server.cpp
@@ -0,0 +1,13 @@
+#include "/src/utils/crow_all.h"
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")
+    ([]() {
+        return "Hello world!";
+    });
+
+    app.port(1111).run();
+}
