f0313fccbe343d5c4aeedc776a8eb45825548e03
YicongY <able1994@hotmail.com>
2018-04-29 18:09:50 -0500

    add

--- server.py
+++ server.py
@@ -35,6 +35,7 @@ def render():
 def senddata():
     json_data = request.get_json()
     raw = json_data.get('text')
+    print(raw)
     #dep_text = '0_4_det\n1_4_compound\n2_4_compound\n3_7_nsubj\n4_6_case\n5_4_nmod: without\n6_0_root\n7_19_mark\n8_10_case\n9_19_nmod: since\n10_19_punct\n11_14_advmod\n12_12_mwe\n13_16_nummod\n14_16_amod\n15_19_nsubjpass\n16_19_aux\n17_19_auxpass\n18_7_ccomp\n19_19_cc\n20_7_ccomp\n21_24_case\n22_24_compound\n23_19_nmod: at\n24_27_case\n25_27_det\n26_24_nmod: in\n27_29_case\n28_27_nmod: of\n29_29_punct\n30_29_appos\n31_7_punct\n0_3_det\n1_3_amod\n2_5_nsubj\n3_5_aux\n4_11_ccomp\n5_8_nsubj\n6_8_aux\n7_5_ccomp\n8_8_advmod\n9_11_punct\n10_0_root\n11_14_compound\n12_14_compound\n13_11_nsubj\n14_14_punct\n15_14_appos\n16_18_case\n17_16_nmod: of\n18_11_punct\n'
     #token_text = '18 2632 421 1310 1895 376 427 2 1524 1219 17 147 156 19160 24653 438 216 10 4870 42 10418 28 153974 1271 26 18 468 4 24820 17 56999 60\n18 465 438 554 1018 14 10 1448 473 17 427 696 8884 1033 17 880 4 5137 60'
     #pos_text = 'DT\nNN\nNN\nNNS\nIN\nNNS\nVBD\nIN\nIN\nNNP\n,\nJJR\nIN\nCD\nJJ\nNNS\nVBD\nVBN\nVBN\nCC\nVBN\nIN\nNNP\nNNP\nIN\nDT\nNN\nIN\nNNP\n,\nNNP\n.\nDT\nJJ\nNNS\nMD\nVB\nWP\nVBZ\nVBG\nRB\n,\nVBD\nNNP\nNNP\nNNP\n,\nNNP\nIN\nNNP\n.'
