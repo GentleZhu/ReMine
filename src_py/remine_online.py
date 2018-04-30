1fa4cd03d6346e89d5c46aaf679cd2e2b2996e90
YicongY <able1994@hotmail.com>
2018-04-30 18:12:48 -0500

    add remine online

--- src_py/remine_online.py
+++ src_py/remine_online.py
@@ -4,35 +4,35 @@ import nltk,json
 import StringIO
 
 class Solver(object):
-	"""docstring for PreProcessor"""
-	def __init__(self):
-		self.word_cnt = 1
-		self.word_mapping = dict()
-		self.punc_mapping = dict()
-		self.punc = {'.',',','"',"'",'?',':',';','-','!','-lrb-','-rrb-','``',"''", ''}
-		self.fdep = ''
-		self.fpos = ''
-		self.fdoc = ''
-		self.fems = ''
-		self.test_tokens = []
-		self.test_words = []
-	def case(self, w):
-		if w.isupper() or w in self.punc:
-			return '3'
-		elif w[0].isupper():
-			return '1'
-		elif w.isdigit():
-			return '4'
-		else:
-			return '0'
+    """docstring for PreProcessor"""
+    def __init__(self):
+        self.word_cnt = 1
+        self.word_mapping = dict()
+        self.punc_mapping = dict()
+        self.punc = {'.',',','"',"'",'?',':',';','-','!','-lrb-','-rrb-','``',"''", ''}
+        self.fdep = ''
+        self.fpos = ''
+        self.fdoc = ''
+        self.fems = ''
+        self.test_tokens = []
+        self.test_words = []
+    def case(self, w):
+        if w.isupper() or w in self.punc:
+            return '3'
+        elif w[0].isupper():
+            return '1'
+        elif w.isdigit():
+            return '4'
+        else:
+            return '0'
 
-	def inWordmapping(self,word):
-		if word not in self.word_mapping:
-			self.word_mapping[word]=self.word_cnt
-			if word in self.punc:
-				self.punc_mapping[self.word_cnt] = word
-			#self.reverse_mapping[self.word_cnt]=word
-			self.word_cnt+=1
+    def inWordmapping(self,word):
+        if word not in self.word_mapping:
+            self.word_mapping[word]=self.word_cnt
+            if word in self.punc:
+                self.punc_mapping[self.word_cnt] = word
+            #self.reverse_mapping[self.word_cnt]=word
+            self.word_cnt+=1
 
         return str(self.word_mapping[word])
 
@@ -156,197 +156,197 @@ class Solver(object):
         self.fems = emsIO.getvalue()
 
         def tokenized_test(self, docIn, posIn, depIn):
-            
-		#with open(docIn, encoding='utf-8') as doc, open(posIn, encoding='utf-8') as pos, open(depIn, encoding='utf-8') as dep:
-		docin = docIn.split('\n').rstrip()
-		posin = posIn.split('\n').rstrip()
-		depin = depIn.split('\n').rstrip()
 
-		depIO = StringIO.StringIO()
-		posIO = StringIO.StringIO()
-		docIO = StringIO.StringIO()
+        #with open(docIn, encoding='utf-8') as doc, open(posIn, encoding='utf-8') as pos, open(depIn, encoding='utf-8') as dep:
+        docin = docIn.split('\n').rstrip()
+        posin = posIn.split('\n').rstrip()
+        depin = depIn.split('\n').rstrip()
 
-		for t,p,d in zip(docin,posin,depin):
-			_word = []
-			_token = []
-			tokens = t.strip().split(' ')
-			postags = p.strip().split(' ')
-			deps = d.strip().split(' ')
-			tmp = []
-			for i,d in enumerate(deps):
-				#dd=d.split('_')
-				tmp.append(str(i)+'_'+d)
-			assert(len(tokens) == len(postags) == len(deps))
-			#print(tokens)
-			for i,w in enumerate(tokens):
-				if i != len(tokens) - 1:
-					docIO.write(self.inWordmapping(w) + ' ')
-					_token.append(self.inWordmapping(w))
-				else:
-					#if w not in self.punc:
-					docIO.write(self.inWordmapping(w) + '\n')
-					_token.append(self.inWordmapping(w))
-					#else:
-						#fdoc.write(w+'\n')
-						#_token.append(w)
-				_word.append(w)
-			self.test_tokens.append(_token)
-			self.test_words.append(_word)
+        depIO = StringIO.StringIO()
+        posIO = StringIO.StringIO()
+        docIO = StringIO.StringIO()
 
-			depIO.write(('\n'.join(tmp)) + '\n')
-			posIO.write(('\n'.join(postags)) + '\n')
+        for t,p,d in zip(docin,posin,depin):
+            _word = []
+            _token = []
+            tokens = t.strip().split(' ')
+            postags = p.strip().split(' ')
+            deps = d.strip().split(' ')
+            tmp = []
+            for i,d in enumerate(deps):
+                #dd=d.split('_')
+                tmp.append(str(i)+'_'+d)
+            assert(len(tokens) == len(postags) == len(deps))
+            #print(tokens)
+            for i,w in enumerate(tokens):
+                if i != len(tokens) - 1:
+                    docIO.write(self.inWordmapping(w) + ' ')
+                    _token.append(self.inWordmapping(w))
+                else:
+                    #if w not in self.punc:
+                    docIO.write(self.inWordmapping(w) + '\n')
+                    _token.append(self.inWordmapping(w))
+                    #else:
+                        #fdoc.write(w+'\n')
+                        #_token.append(w)
+                _word.append(w)
+            self.test_tokens.append(_token)
+            self.test_words.append(_word)
 
-		self.fdoc = docIO.getvalue()
-		self.fdep = depIO.getvalue()
-		self.fpos = posIO.getvalue()
+            depIO.write(('\n'.join(tmp)) + '\n')
+            posIO.write(('\n'.join(postags)) + '\n')
 
-
-	
+        self.fdoc = docIO.getvalue()
+        self.fdep = depIO.getvalue()
+        self.fpos = posIO.getvalue()
 
 
-	def dump_rm(self):
-		_pickle.dump(self.test_words, open('tmp_remine/rm_test_words.p', 'wb'))
-		_pickle.dump(self.test_tokens, open('tmp_remine/rm_test_tokens.p', 'wb'))
-
-	def dump_test(self):
-		_pickle.dump(self.test_words, open('tmp_remine/real_test_words.p', 'wb'))
-		_pickle.dump(self.test_tokens, open('tmp_remine/real_test_tokens.p', 'wb'))
-
-	def dump(self):
-		with open('tmp_remine/tokenized_punctuations.txt','w', encoding='utf-8') as OUT:
-			for k,v in self.punc_mapping.items():
-				OUT.write(str(k)+'\t'+v+'\n')
-		with open('tmp_remine/token_mapping.txt','w', encoding='utf-8') as OUT:
-			for k,v in self.word_mapping.items():
-				OUT.write(str(v)+'\t'+k+'\n')
-		_pickle.dump(self.word_mapping, open('tmp_remine/token_mapping.p', 'wb'))
-		_pickle.dump(self.test_words, open('tmp_remine/test_words.p', 'wb'))
-		_pickle.dump(self.test_tokens, open('tmp_remine/test_tokens.p', 'wb'))
-
-	def load(self):
-		self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
-
-	def load_all(self):
-		self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
-		self.test_tokens = _pickle.load(open('tmp_remine/test_tokens.p', 'rb'))
-		self.test_words = _pickle.load(open('tmp_remine/test_words.p', 'rb'))
-
-	def load_test(self):
-		self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
-		self.test_tokens = _pickle.load(open('tmp_remine/real_test_tokens.p', 'rb'))
-		self.test_words = _pickle.load(open('tmp_remine/real_test_words.p', 'rb'))
-
-	def tokenize(self, docIn, docOut):
-		with open(docIn, encoding='utf-8') as doc, open(docOut,'w', encoding='utf-8') as out:
-			for line in doc:
-				tmp = line.strip().split(' ')
-				out.write(' '.join(list(map(lambda x: self.inWordmapping(x), tmp)))+'\n')
 
 
-	def mapBackv2(self,seg_path):
-		queue=[]
-		r_ptr=0
-		c_ptr=0
-		start=['<None>','<EP>','<RP>','<BP>']
-		end=['</None>','</EP>','</RP>', '</BP>']
-		start_phrase=False
-		output = StringIO.StringIO()
-		list_seg_path = seg_path.split('\n').rstrip()
-	
-		for line in list_seg_path:
-			for token in line.strip().split(' '):
-				queue.append(token)
-			#print queue
-			while (len(queue)>0):
-				#print c_ptr,r_ptr
-				if queue[0] in start or queue[0] in end:
-					#if queue[0] == '</phrase>' or c_ptr < len(self.test_token[r_ptr]):
-					if queue[0] in start and c_ptr == len(self.test_tokens[r_ptr]):
-						#OUT.write('\n'+queue.pop(0)+' ')
-						start_phrase=True
-						output = output.write('\n')
-						r_ptr+=1
-						c_ptr=0
-						queue.pop(0)
-						continue
-					else:
-						if queue[0] in start:
-							start_phrase=True
-						else:
-							start_phrase=False
-							if 'None' in queue[0]:
-								#OUT.write(':BP,')
-								output.write(']_[')
-							elif 'EP' in queue[0]:
-								output.write(':EP]_[')
-							elif 'RP' in queue[0]:
-								ouput.write(':RP]_[')
-							elif 'BP' in queue[0]:
-								output.write(':BP]_[')
-						queue.pop(0)
-				elif c_ptr < len(self.test_tokens[r_ptr]) and queue[0] == self.test_tokens[r_ptr][c_ptr]:
-					if start_phrase:
-						output.write(self.test_words[r_ptr][c_ptr] + ' ')
-					else:
-						output.write(self.test_words[r_ptr][c_ptr] + ']_[')
-					queue.pop(0)
-					c_ptr+=1
-				else:
-					r_ptr+=1
-					c_ptr=0
-					output.write('\n')
-			return output.getvalue()
 
-	def map(self,seg_path,outpath):
-		queue=[]
-		r_ptr=0
-		c_ptr=0
-		start=['<None>','<EP>','<RP>','<BP>']
-		end=['</None>','</EP>','</RP>', '</BP>']
-		start_phrase=False
-		with open(seg_path,'r', encoding='utf-8') as _seg, open(outpath,'w', encoding='utf-8') as OUT:
-			for line in _seg:
-				for token in line.strip().split(' '):
-					queue.append(token)
-				#print queue
-				while (len(queue)>0):
-					#print c_ptr,r_ptr
-					if queue[0] in start or queue[0] in end:
-						#if queue[0] == '</phrase>' or c_ptr < len(self.test_token[r_ptr]):
-						if queue[0] in start and c_ptr == len(self.test_tokens[r_ptr]):
-							#OUT.write('\n'+queue.pop(0)+' ')
-							start_phrase=True
-							OUT.write('\n')
-							r_ptr+=1
-							c_ptr=0
-							queue.pop(0)
-							continue
-						else:
-							if queue[0] in start:
-								start_phrase=True
-							else:
-								start_phrase=False
-								if 'None' in queue[0]:
-									#OUT.write(':BP,')
-									OUT.write(']_[')
-								elif 'EP' in queue[0]:
-									OUT.write(':EP]_[')
-								elif 'RP' in queue[0]:
-									OUT.write(':RP]_[')
-								elif 'BP' in queue[0]:
-									OUT.write(':BP]_[')
-							queue.pop(0)
-					elif c_ptr < len(self.test_tokens[r_ptr]) and queue[0] == self.test_tokens[r_ptr][c_ptr]:
-						if start_phrase:
-							OUT.write(self.test_words[r_ptr][c_ptr] + ' ')
-						else:
-							OUT.write(self.test_words[r_ptr][c_ptr] + ']_[')
-						queue.pop(0)
-						c_ptr+=1
-					else:
-						r_ptr+=1
-						c_ptr=0
-						OUT.write('\n')
+    def dump_rm(self):
+        _pickle.dump(self.test_words, open('tmp_remine/rm_test_words.p', 'wb'))
+        _pickle.dump(self.test_tokens, open('tmp_remine/rm_test_tokens.p', 'wb'))
+
+    def dump_test(self):
+        _pickle.dump(self.test_words, open('tmp_remine/real_test_words.p', 'wb'))
+        _pickle.dump(self.test_tokens, open('tmp_remine/real_test_tokens.p', 'wb'))
+
+    def dump(self):
+        with open('tmp_remine/tokenized_punctuations.txt','w', encoding='utf-8') as OUT:
+            for k,v in self.punc_mapping.items():
+                OUT.write(str(k)+'\t'+v+'\n')
+        with open('tmp_remine/token_mapping.txt','w', encoding='utf-8') as OUT:
+            for k,v in self.word_mapping.items():
+                OUT.write(str(v)+'\t'+k+'\n')
+        _pickle.dump(self.word_mapping, open('tmp_remine/token_mapping.p', 'wb'))
+        _pickle.dump(self.test_words, open('tmp_remine/test_words.p', 'wb'))
+        _pickle.dump(self.test_tokens, open('tmp_remine/test_tokens.p', 'wb'))
+
+    def load(self):
+        self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
+
+    def load_all(self):
+        self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
+        self.test_tokens = _pickle.load(open('tmp_remine/test_tokens.p', 'rb'))
+        self.test_words = _pickle.load(open('tmp_remine/test_words.p', 'rb'))
+
+    def load_test(self):
+        self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
+        self.test_tokens = _pickle.load(open('tmp_remine/real_test_tokens.p', 'rb'))
+        self.test_words = _pickle.load(open('tmp_remine/real_test_words.p', 'rb'))
+
+    def tokenize(self, docIn, docOut):
+        with open(docIn, encoding='utf-8') as doc, open(docOut,'w', encoding='utf-8') as out:
+            for line in doc:
+                tmp = line.strip().split(' ')
+                out.write(' '.join(list(map(lambda x: self.inWordmapping(x), tmp)))+'\n')
+
+
+    def mapBackv2(self,seg_path):
+        queue=[]
+        r_ptr=0
+        c_ptr=0
+        start=['<None>','<EP>','<RP>','<BP>']
+        end=['</None>','</EP>','</RP>', '</BP>']
+        start_phrase=False
+        output = StringIO.StringIO()
+        list_seg_path = seg_path.split('\n').rstrip()
+
+        for line in list_seg_path:
+            for token in line.strip().split(' '):
+                queue.append(token)
+            #print queue
+            while (len(queue)>0):
+                #print c_ptr,r_ptr
+                if queue[0] in start or queue[0] in end:
+                    #if queue[0] == '</phrase>' or c_ptr < len(self.test_token[r_ptr]):
+                    if queue[0] in start and c_ptr == len(self.test_tokens[r_ptr]):
+                        #OUT.write('\n'+queue.pop(0)+' ')
+                        start_phrase=True
+                        output = output.write('\n')
+                        r_ptr+=1
+                        c_ptr=0
+                        queue.pop(0)
+                        continue
+                    else:
+                        if queue[0] in start:
+                            start_phrase=True
+                        else:
+                            start_phrase=False
+                            if 'None' in queue[0]:
+                                #OUT.write(':BP,')
+                                output.write(']_[')
+                            elif 'EP' in queue[0]:
+                                output.write(':EP]_[')
+                            elif 'RP' in queue[0]:
+                                ouput.write(':RP]_[')
+                            elif 'BP' in queue[0]:
+                                output.write(':BP]_[')
+                        queue.pop(0)
+                elif c_ptr < len(self.test_tokens[r_ptr]) and queue[0] == self.test_tokens[r_ptr][c_ptr]:
+                    if start_phrase:
+                        output.write(self.test_words[r_ptr][c_ptr] + ' ')
+                    else:
+                        output.write(self.test_words[r_ptr][c_ptr] + ']_[')
+                    queue.pop(0)
+                    c_ptr+=1
+                else:
+                    r_ptr+=1
+                    c_ptr=0
+                    output.write('\n')
+            return output.getvalue()
+
+    def map(self,seg_path,outpath):
+        queue=[]
+        r_ptr=0
+        c_ptr=0
+        start=['<None>','<EP>','<RP>','<BP>']
+        end=['</None>','</EP>','</RP>', '</BP>']
+        start_phrase=False
+        with open(seg_path,'r', encoding='utf-8') as _seg, open(outpath,'w', encoding='utf-8') as OUT:
+            for line in _seg:
+                for token in line.strip().split(' '):
+                    queue.append(token)
+                #print queue
+                while (len(queue)>0):
+                    #print c_ptr,r_ptr
+                    if queue[0] in start or queue[0] in end:
+                        #if queue[0] == '</phrase>' or c_ptr < len(self.test_token[r_ptr]):
+                        if queue[0] in start and c_ptr == len(self.test_tokens[r_ptr]):
+                            #OUT.write('\n'+queue.pop(0)+' ')
+                            start_phrase=True
+                            OUT.write('\n')
+                            r_ptr+=1
+                            c_ptr=0
+                            queue.pop(0)
+                            continue
+                        else:
+                            if queue[0] in start:
+                                start_phrase=True
+                            else:
+                                start_phrase=False
+                                if 'None' in queue[0]:
+                                    #OUT.write(':BP,')
+                                    OUT.write(']_[')
+                                elif 'EP' in queue[0]:
+                                    OUT.write(':EP]_[')
+                                elif 'RP' in queue[0]:
+                                    OUT.write(':RP]_[')
+                                elif 'BP' in queue[0]:
+                                    OUT.write(':BP]_[')
+                            queue.pop(0)
+                    elif c_ptr < len(self.test_tokens[r_ptr]) and queue[0] == self.test_tokens[r_ptr][c_ptr]:
+                        if start_phrase:
+                            OUT.write(self.test_words[r_ptr][c_ptr] + ' ')
+                        else:
+                            OUT.write(self.test_words[r_ptr][c_ptr] + ']_[')
+                        queue.pop(0)
+                        c_ptr+=1
+                    else:
+                        r_ptr+=1
+                        c_ptr=0
+                        OUT.write('\n')
 
 
 
