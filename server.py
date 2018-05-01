325ee16789e3fa78705df52a1fb1ba0c6caf4abb
YicongY <able1994@hotmail.com>
2018-05-01 12:33:29 -0500

    add

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ def senddata():
         print('parse', sentence.depparse())
 
         for token in sentence:
-            print('parse',token.depparse())
+            #print('parse',token.depparse())
             #print('pos',token.pos)
             #dep_text = dep_text + '\n' + token.dep
             # token_text = token_text + '\n' + token.lemma
@@ -72,7 +72,7 @@ def senddata():
             # dep_text = dep_text + '\n' + token.depparse
             token_text.write(token.lemma + '\n')
             pos_text.write(token.pos + '\n')
-            #dep_text.write(token.depparse() + '\n')
+            dep_text.write(token.depparse + '\n')
 
     #remine-ie.sh
     dep_text = dep_text.getvalue()
@@ -120,7 +120,7 @@ if __name__=='__main__':
     #pane.send_keys('cd /shared/data/remine/CS512_Website/Remine', enter = True)
     #pane.send_keys('./bin/remine --model pre_train/segmentation.model --mode 1', enter=True)
     #global NLP_client
-    NLP_client = CoreNLPClient(server='http://dmserv5.cs.illinois.edu:9000',default_annotators=['depparse', 'lemma', 'pos'])
+    NLP_client = CoreNLPClient(server='http://dmserv4.cs.illinois.edu:9002',default_annotators=['depparse', 'lemma', 'pos'])
    # NLP_clinet = pywrap.CoreNLP(url='http://localhost:9002', annotator_list=["lemma","ner","depparse","pos"])
 
     http_server = WSGIServer(('0.0.0.0', 1111), app)
