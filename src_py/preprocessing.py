5985fa21c033c8960d9d35d7e8911df02cc5a9de
YicongY <able1994@hotmail.com>
2018-05-01 17:00:07 -0500

    add

--- src_py/preprocessing.py
+++ src_py/preprocessing.py
@@ -212,7 +212,7 @@ class PreProcessor(object):
         _pickle.dump(self.test_tokens, open('tmp_remine/test_tokens.p', 'wb'))
 
     def load(self):
-        self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
+        self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb',protocol = 2))
 
     def load_all(self):
         self.word_mapping = _pickle.load(open('tmp_remine/token_mapping.p', 'rb'))
