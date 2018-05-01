4f685980ba4b22f91d56d78ab9a1b7cb8485b67f
YicongY <able1994@hotmail.com>
2018-05-01 14:39:22 -0500

    add

--- server.py
+++ server.py
@@ -110,7 +110,7 @@ def senddata():
     print(remine_segmentation)
     remine_seg_out = answer.mapBackv2(remine_segmentation)
     answer.extract_transformat(remine_seg_out, token_text, pos_text)
-
+    print(answer.fems)
     response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 1})
     remine_segmentation = response.text
 
