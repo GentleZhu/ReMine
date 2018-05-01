c9632c363ae95eea08766a14f87c252bdcdf12f7
YicongY <able1994@hotmail.com>
2018-05-01 16:53:34 -0500

    add

--- server.py
+++ server.py
@@ -109,26 +109,26 @@ def senddata():
 
     response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 0})
     remine_segmentation = response.text
-    #print("remine_segement",remine_segmentation)
-    remine_seg_out = answer.mapBackv2(remine_segmentation)
-    check = answer.extract_transformat(remine_seg_out, token_text, pos_text)
-    assert check == 1
-    #print('ems:\n', answer.fems)
-    print(answer.fdoc)
-    #print(answer.fpos)
-    #print(answer.fdep)
-    print(answer.fems)
-    response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 1})
-    remine_segmentation = response.text
-
-    #result = answer.translate(remine_segmentation)
-
-    #print(result)
-    #print(remine_segmentation)
-    #with open("result.txt","w") as f:
-        #f.write(result)
-
-    return jsonify({'tuple': remine_segmentation})
+    print("remine_segement",remine_segmentation)
+    # remine_seg_out = answer.mapBackv2(remine_segmentation)
+    # check = answer.extract_transformat(remine_seg_out, token_text, pos_text)
+    # assert check == 1
+    # #print('ems:\n', answer.fems)
+    # print(answer.fdoc)
+    # #print(answer.fpos)
+    # #print(answer.fdep)
+    # print(answer.fems)
+    # response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 1})
+    # remine_segmentation = response.text
+    #
+    # #result = answer.translate(remine_segmentation)
+    #
+    # #print(result)
+    # #print(remine_segmentation)
+    # #with open("result.txt","w") as f:
+    #     #f.write(result)
+    #
+    # return jsonify({'tuple': remine_segmentation})
 
 
 
