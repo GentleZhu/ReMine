e37924652acea37326a35a7c0348d06549587a16
YicongY <able1994@hotmail.com>
2018-04-30 22:01:50 -0500

    add

--- server.py
+++ server.py
@@ -75,9 +75,9 @@ def senddata():
     answer = Solver()
     answer.load()
     answer.tokenized_test(token_text, pos_text, dep_text)
-    print(answer.fdoc)
-    print(answer.fpos)
-    print(answer.fdep)
+    #print(answer.fdoc)
+    #print(answer.fpos)
+    #print(answer.fdep)
     # print(answer.fems)
     response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 0})
     remine_segmentation = response.text
