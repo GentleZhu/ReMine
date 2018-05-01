32b2e62fccfc4efd1c229dcb529c29b596d178a7
YicongY <able1994@hotmail.com>
2018-04-30 19:30:55 -0500

    add remine online

--- server.py
+++ server.py
@@ -77,6 +77,7 @@ def senddata():
     answer.tokenized_test(token_text, pos_text, dep_text)
     response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 0})
     remine_segmentation = response.text
+    print(remine_segmentation)
     remine_seg_out = answer.mapBackv2(remine_segmentation)
     answer.extract_transformat(remine_seg_out, token_text, pos_text)
     response = requests.get('http://dmserv4.cs.illinois.edu:10086/pass_result', json ={"pos": answer.fpos, "tokens": answer.fdoc, "dep": answer.fdep, "ent": answer.fems, "mode": 0})
