2d2054a559d60a43739d329046bda3418fa5021b
YicongY <able1994@hotmail.com>
2018-05-06 16:34:02 -0500

    add

--- server.py
+++ server.py
@@ -169,7 +169,7 @@ if __name__=='__main__':
     model1 = Model('tmp_remine/token_mapping.p')
 
     NLP_client = CoreNLPClient(server='http://dmserv4.cs.illinois.edu:9000',default_annotators=['depparse', 'lemma', 'pos'])
-
-    http_server = WSGIServer(('0.0.0.0', 1111), app)
-
-    http_server.serve_forever()
+    app.run()
+    # http_server = WSGIServer(('0.0.0.0', 1111), app)
+    #
+    # http_server.serve_forever()
