e06ff2ddd4ea921fa3018823082f044279b49292
YicongY <able1994@hotmail.com>
2018-05-06 17:11:38 -0500

    add

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from ReMine import app
 
 if __name__ == "__main__":
+    global coref
+    coref = Coref()
+    model1 = Model('tmp_remine/token_mapping.p')
+    global NLP_client
+    NLP_client = CoreNLPClient(server='http://dmserv4.cs.illinois.edu:9000',
+                               default_annotators=['depparse', 'lemma', 'pos'])
     app.run()
